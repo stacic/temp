6650a492acb269809c5af09270d5294bf5622a0b
Staci Cooper <smcooper@us.ibm.com>
2014-08-12 17:08:39 -0400

    Add sample bb proj with alert

---
+++ alertTest/platforms/blackberry10/www/cordova_plugins.js
@@ -0,0 +1,24 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/blackberry10/beep.js",
+        "id": "org.apache.cordova.dialogs.beep",
+        "clobbers": [
+            "window.navigator.notification.beep"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.dialogs": "0.2.9"
+}
+// BOTTOM OF METADATA
+});
